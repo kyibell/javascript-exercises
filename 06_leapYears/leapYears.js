d6479317a9f550727955c995c7555bf4f28528ba
kyibell <148383705+kyibell@users.noreply.github.com>
2024-07-03 17:54:57 -0500

    Completed javascript excercises 1-7.

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,14 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    const divFour = year % 4 === 0;
+    const century = year % 100 === 0;
+    const divFourHundred = year % 400 === 0;
 
+    if (divFour && !century || divFourHundred ) {
+        return true;
+    }
+    else {
+        return false;
+    }
 };
 
 // Do not edit below this line
