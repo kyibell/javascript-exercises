5927058023692f294f71bd959cbace43200ae54b
kyibell <148383705+kyibell@users.noreply.github.com>
2024-07-03 16:31:42 -0500

    completed remove from array.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,6 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...target) {
+    return arr.filter(val => !target.includes(val));
+
 };
 
 // Do not edit below this line
